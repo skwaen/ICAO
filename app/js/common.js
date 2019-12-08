6d1db507330afe357e886583143d96c189f6db9d
Nikita <skwaen@gmail.com>
2019-12-08 19:26:06 +0200

    use lower case

--- app/js/common.js
+++ app/js/common.js
@@ -1,58 +1,32 @@
 $(function() {
 
 	var alphabetIcao = {
-		A: 'Alfa',
 		a: 'Alfa',
-		B: 'Bravo',
 		b: 'Bravo',
-		C: 'Charlie',
 		c: 'Charlie',
-		D: 'Delta',
 		d: 'Delta',
-		E: 'Echo',
 		e: 'Echo',
-		F: 'Foxtrot',
 		f: 'Foxtrot',
-		G: 'Golf',
 		g: 'Golf',
-		H: 'Hotel',
 		h: 'Hotel',
-		I: 'India',
 		i: 'India',
-		J: 'Juliett',
 		j: 'Juliett',
-		K: 'Kilo',
 		k: 'Kilo',
-		L: 'Lima',
 		l: 'Lima',
-		M: 'Mike',
 		m: 'Mike',
-		N: 'November',
 		n: 'November',
-		O: 'Oscar',
 		o: 'Oscar',
-		P: 'Papa',
 		p: 'Papa',
-		Q: 'Quebec',
 		q: 'Quebec',
-		R: 'Romeo',
 		r: 'Romeo',
-		S: 'Sierra',
 		s: 'Sierra',
-		T: 'Tango',
 		t: 'Tango',
-		U: 'Uniform',
 		u: 'Uniform',
-		V: 'Victor',
 		v: 'Victor',
-		W: 'Whiskey',
 		w: 'Whiskey',
-		X: 'X-ray',
 		x: 'X-ray',
-		Y: 'Yankee',
 		y: 'Yankee',
-		Z: 'Zulu',
-		x: 'Zulu',
+		z: 'Zulu',
 		0: 'Zero',
 		1: 'One',
 		2: 'Two',
@@ -75,6 +49,8 @@ $(function() {
 
 	for (var i = 0; i < splitedStringArr.length; i++) {	// инициадизируем проход по массиву
 		var splitedValue = splitedStringArr[i];	// перем каждое значение элемента массива по его индексу
+
+		splitedValue = splitedValue.toLowerCase(); // преоразовываем в нижний регистр
 
 		var decodedValue = alphabetIcao[splitedValue];
 		console.log('decodedValue', decodedValue);
